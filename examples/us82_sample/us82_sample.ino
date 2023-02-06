fc07745f1f0d9f28903374aaea392323d880c299
Phuwasit <phuwasit@p-robotics.net>
2023-02-06 19:32:04 +0700

    new Example

--- examples/us82_sample/us82_sample.ino
+++ examples/us82_sample/us82_sample.ino
@@ -7,7 +7,7 @@ void setup() {
   Serial.begin(115200);
   
   SW_press(); // Wair For Press SW
-  
+
   motor(1, 100);  // motor 1 forward maximun speed is 100
   motor(2, 100);  // motor 2 forward maximun speed is 100
   delay(1000);
@@ -39,6 +39,8 @@ void setup() {
 
   tr(100);        // turn right maximun speed is 100
   delay(1000);
+
+  motor_stop(ALL); // stop all motor
   
 }
 
@@ -51,6 +53,7 @@ void loop() {
     Serial.print(Value);
     Serial.print("\t");
   }
+  Serial.println("");
   for (int pins = 0; pins < 8; pins++){
     Value = in(pins);  // read Digital value form pin With "in" command 0,1,2...7 if use "digitalRead" pins is 14,15,16..19
     Serial.print(Value);
